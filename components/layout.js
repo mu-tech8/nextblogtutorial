4d4fe8b852ba617b2725107156779efb7225b9b6
mu-tech8 <kawamuratakuya@mu.local>
2021-09-05 17:54:16 +0900

    修正

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
-import Image from "next/Image";
+import Image from "next/image";
 
 const name = "かわむ";
 export const siteTitle = "my site";
